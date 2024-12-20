b735a2ab8a7b9cf27290a77950b3bbc5bfb5b969
Quoc Truong <quoct@google.com>
2024-12-20 13:12:40 -0800

    add packages

--- packages/k8s/src/hooks/run-script-step.ts
+++ packages/k8s/src/hooks/run-script-step.ts
@@ -23,18 +23,19 @@ export async function runScriptStep(
   args.entryPoint = 'sh'
   args.entryPointArgs = ['-e', containerPath]
   try {
-    core.debug("quoct execing pod step");
+    core.debug(`quoct execing pod step ${JSON.stringify(args)}`);
     await execPodStep(
       [args.entryPoint, ...args.entryPointArgs],
       state.jobPod,
       JOB_CONTAINER_NAME
     )
-    core.debug("quoct done execing pod step");
+    core.debug(`quoct done execing pod step ${JSON.stringify(args)}`);
   } catch (err) {
-    core.debug(`execPodStep failed: ${JSON.stringify(err)}`)
+    core.debug(`quoct execPodStep for ${JSON.stringify(args)} failed: ${JSON.stringify(err)}`)
     const message = (err as any)?.response?.body?.message || err
     throw new Error(`failed to run script step: ${message}`)
   } finally {
+    core.debug(`quoct getting to finally ${JSON.stringify(args)}`)
     fs.rmSync(runnerPath)
   }
 }
